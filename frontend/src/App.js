749bc481e109f78ad4369b81840e188d41c4b441
Alex <s0351575@salemstate.edu>
2023-02-21 09:54:56 -0500

    added alex Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import LandingPage from "./components/pages/landingPage";
 import HomePage from "./components/pages/homePage";
 import Login from "./components/pages/loginPage";
 import MbtaServicePage from "./components/pages/mbtaService";
+import AlexTest from "./components/pages/alexTest";
 
 import Signup from "./components/pages/registerPage";
 import PrivateUserProfile from "./components/pages/privateUserProfilePage";
@@ -33,6 +34,7 @@ const App = () => {
           <Route exact path="/home" element={<HomePage />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/alexTest" elements={<AlexTest />} />
           <Route exact path="/mbtaService" element={<MbtaServicePage />} />
           <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
         </Routes>
